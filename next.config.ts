76c7e50bc282173a1adde3026a8a52d1f1e6220f
lmontero18 <lmonterom879@gmail.com>
2025-05-22 15:56:10 -0600

    legoss

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withPWA = withPWAInit({
+  dest: "public",
+});
 
-export default nextConfig;
+export default withPWA({
+  reactStrictMode: true,
+});
